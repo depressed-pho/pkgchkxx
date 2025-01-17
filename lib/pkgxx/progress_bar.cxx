d9dcc89f0d2e91a4ec79691cad9c03344761d5f2
PHO <pho@cielonegro.org>
2025-01-17 23:22:09 +0900

    new class: pkgxx::progress_bar

---
+++ lib/pkgxx/progress_bar.cxx
@@ -0,0 +1,250 @@
+#include "config.h"
+
+#include <cassert>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+#if defined(HAVE_SYS_IOCTL_H)
+#  include <sys/ioctl.h>
+#endif
+#include <unistd.h>
+
+#include "progress_bar.hxx"
+
+namespace pkgxx {
+    progress_bar::progress_bar(
+        int,
+        std::size_t total,
+        double decay_p,
+        bool show_percent,
+        bool show_ETA,
+        bar_style style)
+        : _decay_p(decay_p)
+        , _show_percent(show_percent)
+        , _show_ETA(show_ETA)
+        , _style(std::move(style))
+        , _out(STDERR_FILENO, false)
+        , _term_width(term_width(_out))
+        , _last_updated(std::chrono::steady_clock::now())
+        , _total(total)
+        , _done(0)
+          // _weight is a function of _total and decay_p. Computing it
+          // isn't so cheap so we cache it.
+        , _weight(std::exp(-1 / (static_cast<double>(_total) * _decay_p))) {
+
+        if (_decay_p <= 0) {
+            throw std::runtime_error(
+                "decay_p must be positive and should not be greater than 1");
+        }
+#if defined(HAVE_DECL_SIGWINCH)
+        if (_term_width) {
+            // SIGWINCH exists everywhere in practice, but it's
+            // nevertheless non-standard.
+            _winch_handler = std::make_unique<scoped_signal_handler>(
+                std::initializer_list<int> {SIGWINCH},
+                [this](int) {
+                    lock_t lk(_mtx);
+                    _term_width = term_width(_out);
+                    redraw();
+                });
+        }
+#endif
+        redraw();
+    }
+
+    progress_bar::~progress_bar() {
+        // We must destroy the SIGWINCH handler first, otherwise there can
+        // be a race condition.
+        _winch_handler.reset();
+
+        if (_term_width) {
+            _out << '\r'   // Move cursor to column 0.
+                 << "\e[K" // Erase from cursor to the end of line.
+                 << std::flush;
+        }
+    }
+
+    progress_bar&
+    progress_bar::operator+= (std::size_t delta) {
+        lock_t lk(_mtx);
+        return *this = _done + delta;
+    }
+
+    progress_bar&
+    progress_bar::operator= (std::size_t done) {
+        lock_t lk(_mtx);
+
+        if (done < _done) {
+            throw std::runtime_error("progress must be monotonically increasing");
+        }
+        else if (done == _done) {
+            return *this; // no-op
+        }
+        else if (done > _total) {
+            throw std::runtime_error("progress must not be greater than total");
+        }
+
+        auto const now      = std::chrono::steady_clock::now();
+        auto const slowness = // in seconds
+            static_cast<double>(_total) *
+            std::chrono::duration<double>(now - _last_updated).count();
+
+        if (!_slowness_EST) {
+            _slowness_EST = slowness;
+        }
+        else {
+            _slowness_EST = *_slowness_EST * _weight + slowness * (1.0 - _weight);
+        }
+        _done         = done;
+        _last_updated = now;
+
+        redraw();
+        return *this;
+    }
+
+    void
+    progress_bar::redraw() {
+        if (!_term_width) {
+            return;
+        }
+
+        if (_show_percent) {
+            auto const pct = format_percentage();
+
+            if (_show_ETA) {
+                redraw({format_percentage(), format_ETA()});
+            }
+            else {
+                redraw({format_percentage()});
+            }
+        }
+        else {
+            if (_show_ETA) {
+                redraw({format_ETA()});
+            }
+            else {
+                redraw({});
+            }
+        }
+    }
+
+    void
+    progress_bar::redraw(std::initializer_list<std::string> const& postfix) {
+        assert(_term_width);
+
+        std::size_t bar_width = *_term_width;
+        for (auto const& elem: postfix) {
+            if (bar_width < 1 + elem.length()) {
+                // The terminal is too narrow.
+                _out << '\r'   // Move cursor to column 0.
+                     << "\e[K" // Erase from cursor to the end of line.
+                     << std::flush;
+                return;
+            }
+            else {
+                bar_width -= 1 + elem.length();
+            }
+        }
+
+        std::string const bar = format_bar(bar_width);
+        _out << '\r' // Move cursor to column 0.
+             << bar; // But don't erase the line. Just overwrite it.
+        for (auto const& elem: postfix) {
+            _out << ' ' << elem;
+        }
+        _out << std::flush;
+    }
+
+    double
+    progress_bar::progress() const {
+        if (_total == 0) {
+            return 1.0; // special case
+        }
+        else {
+            return static_cast<double>(_done) / static_cast<double>(_total);
+        }
+    }
+
+    std::string
+    progress_bar::format_bar(std::size_t width) const {
+        if (width < 2) {
+            // The terminal is too narrow.
+            return "";
+        }
+
+        std::size_t const prog =
+            static_cast<std::size_t>(
+                std::floor(progress() * static_cast<double>(width - 2)));
+
+        std::string bar;
+        bar.resize(width);
+        bar.push_back(_style.begin);
+        for (std::size_t i = 0; i < width - 2; i++) {
+            if (i < prog) {
+                bar.push_back(_style.fill);
+            }
+            else if (i == prog) {
+                bar.push_back(_style.tip);
+            }
+            else {
+                bar.push_back(_style.bg);
+            }
+        }
+        bar.push_back(_style.end);
+        return bar;
+    }
+
+    std::string
+    progress_bar::format_percentage() const {
+        std::stringstream ss;
+        double const pct = progress() * 100;
+        ss << std::setw(3) << std::floor(pct) << '%';
+        return ss.str();
+    }
+
+    std::string
+    progress_bar::format_ETA() const {
+        if (_slowness_EST) {
+            int s = static_cast<int>(std::floor((1.0 - progress()) * *_slowness_EST));
+
+            int const h = s / 60 / 60;
+            s %= 60 * 60;
+
+            int const m = s / 60;
+            s %= 60;
+
+            std::stringstream ss;
+            ss << "(ETA: " << std::setfill('0');
+            if (h > 0) {
+                ss << std::setw(2) << h << ':'
+                   << std::setw(2) << m << ':'
+                   << std::setw(2) << s;
+            }
+            else {
+                ss << std::setw(2) << m << ':'
+                   << std::setw(2) << s;
+            }
+            ss << ')';
+            return ss.str();
+        }
+        else {
+            // We have no data to estimate the remaining time.
+            return std::string(std::strlen("(ETA: HH:MM)"), ' ');
+        }
+    }
+
+    std::optional<std::size_t>
+    progress_bar::term_width(fdostream const& _out) {
+#if defined(HAVE_ISATTY) && defined(HAVE_IOCTL) && defined(TIOCGWINSZ) && \
+    defined(HAVE_STRUCT_WINSIZE_WS_COL)
+        if (auto fd = _out.fd(); isatty(*fd)) {
+            struct winsize ws;
+            if (ioctl(*fd, TIOCGWINSZ, &ws) != -1) {
+                return ws.ws_col;
+            }
+        }
+#endif
+        return std::nullopt;
+    }
+}
---
+++ lib/pkgxx/progress_bar.hxx
@@ -0,0 +1,130 @@
+#pragma once
+
+#include <chrono>
+#include <cstddef>
+#include <initializer_list>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <type_traits>
+
+#include <pkgxx/fdstream.hxx>
+#include <pkgxx/scoped_signal_handler.hxx>
+
+// We know what we are doing! Just don't warn us about these!
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wgnu-string-literal-operator-template"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#include <named-parameters.hpp>
+#pragma GCC diagnostic pop
+
+namespace pkgxx {
+    // I'm not comfortable with bringing it in this scope, but what else
+    // can we do?
+    using namespace na::literals;
+
+    /** A text-based progress bar to be displayed iff stderr is a tty,
+     * based on the algorithm described in
+     * https://stackoverflow.com/a/42009090
+     *
+     * Instances of this class are thread-safe.
+     */
+    struct progress_bar {
+        struct bar_style {
+            char begin = '[';
+            char fill  = '=';
+            char bg    = ' ';
+            char tip   = '>';
+            char end   = ']';
+        };
+
+        /** Creating an instance of \c progress_bar displays a progress
+         * bar. "decay_p" is ignored when "show_ETA" is false.
+         */
+        template <typename... Args>
+        progress_bar(std::size_t total, Args&&... args)
+            : progress_bar(
+                0, total,
+                na::get("decay_p"_na      = 0.1        , std::forward<Args>(args)...),
+                na::get("show_percent"_na = true       , std::forward<Args>(args)...),
+                na::get("show_ETA"_na     = true       , std::forward<Args>(args)...),
+                na::get("bar_style"_na    = bar_style{}, std::forward<Args>(args)...)) {}
+
+        ~progress_bar();
+
+        progress_bar& operator++ ()    { return *this += 1; }
+        progress_bar& operator++ (int) { return *this += 1; }
+        progress_bar& operator+= (std::size_t delta);
+        progress_bar& operator = (std::size_t done);
+
+        /** Print a message. The function \c f is assumed to print a
+         * newline at the end.
+         */
+        template <typename F>
+        void
+        message(F const& f) {
+            lock_t lk(_mtx);
+
+            static_assert(std::is_invocable_v<F, std::ostream&>);
+            static_assert(std::is_same_v<void, std::invoke_result_t<F, std::ostream&>>);
+
+            if (_term_width) {
+                _out << '\r'    // Move cursor to column 0.
+                     << "\e[K"; // Erase from cursor to the end of line.
+            }
+            f(_out);
+            redraw();
+        }
+
+    private:
+        progress_bar(
+            int, // a dummy parameter to avoid conflicting with the public ctor
+            std::size_t total,
+            double decay_p,
+            bool show_percent,
+            bool show_ETA,
+            bar_style style);
+
+        void
+        redraw();
+
+        void
+        redraw(std::initializer_list<std::string> const& postfix);
+
+        double
+        progress() const;
+
+        std::string
+        format_bar(std::size_t width) const;
+
+        std::string
+        format_percentage() const;
+
+        std::string
+        format_ETA() const;
+
+        static std::optional<std::size_t>
+        term_width(fdostream const& _out);
+
+    private:
+        using mutex_t = std::recursive_mutex;
+        using lock_t  = std::lock_guard<mutex_t>;
+
+        mutable mutex_t _mtx;
+        double _decay_p;
+        bool _show_percent;
+        bool _show_ETA;
+        bar_style _style;
+        fdostream _out;
+        // std::nullopt if stderr is not a tty.
+        std::optional<std::size_t> _term_width;
+
+        std::chrono::steady_clock::time_point _last_updated;
+        std::optional<double> _slowness_EST; // in seconds
+        std::size_t _total;
+        std::size_t _done;
+        double _weight;
+
+        std::unique_ptr<scoped_signal_handler> _winch_handler;
+    };
+}
